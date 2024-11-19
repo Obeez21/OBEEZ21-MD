9cabe424864cf843be06fd0457f246bc46a26c73
Obeez21 <134448731+Obeez21@users.noreply.github.com>
2024-11-19 11:07:38 +0200

    Add files via upload

---
+++ commandes/Reboot.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+const {zokou}=require("../framework/zokou")
+
+
+
+
+
+
+
+zokou({nomCom:"restart",categorie:"Mods",reaction:"📴"},async(dest,z,com)=>{
+
+
+  
+const{repondre,ms,dev,superUser}=com;
+
+  if(!superUser)
+  {
+    return repondre("This command is for owner only");
+  }
+
+  const {exec}=require("child_process")
+
+    repondre("OBEEZ21-MD bot Restarting ⚽");
+
+  exec("pm2 restart all");
+  
+
+  
+
+
+
+})
